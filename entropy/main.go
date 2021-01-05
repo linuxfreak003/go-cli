955bbfe4f33b18ccb829a5a49da49d3c24e52aa3
Jared Chapman <jaredpchapman@gmail.com>
2021-01-04 19:58:05 -0700

    Added actual implementation

--- entropy/main.go
+++ entropy/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
 	validator "github.com/lane-c-wagner/go-password-validator"
 )
 
 func main() {
-	entropy := validator.GetEntropy("pass")
-	fmt.Printf("Entropy: %f\n", entropy)
+	pass := strings.Join(os.Args[1:], " ")
+	entropy := validator.GetEntropy(pass)
+	fmt.Printf("Entropy: %0.2f bits\n", entropy)
 }
